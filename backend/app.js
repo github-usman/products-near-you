710e336cc5c7eee275f5aa95c1523fecbb272c9c
github-usman <usmantoyou@gmail.com>
2024-06-27 18:33:44 +0530

    backend : Error handle

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import connectMongoDB from "./database/db.js"
 import  greeting  from "./routes/greeting.route.js"
 import  product  from "./routes/product.route.js"
+import { customErrorMiddleware } from "./middleware/error.js";
 
 connectMongoDB();
 const app = express();
@@ -11,5 +12,9 @@ app.use(bodyParser.json());
 // routes
 app.use("/greeting",greeting);
 app.use("/",product);
+
+
+// Middleware for errors
+app.use(customErrorMiddleware)
 
 export default app;
--- backend/database/db.js
+++ backend/database/db.js
@@ -5,7 +5,7 @@ const connectMongoDB = () => {
     mongoose
         .connect(dbUri, { dbName })
         .then(() => console.log(`DB is connect successfully.`))
-        .catch((err) => console.log(`Unable to connect with DB due to : ${err}`));
+        // .catch((err) => console.log(`Unable to connect with DB due to : ${err}`));
 };
 
 export default connectMongoDB;
