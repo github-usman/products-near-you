2a8a791c52d1a576703f31227b14f36abea6bb8a
github-usman <usmantoyou@gmail.com>
2024-06-27 11:42:00 +0530

    backend crud for product done

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,11 +1,9 @@
 import { Product } from "../models/product.model.js";
 
-// Create a Product
+// Create a Product -------ADMIN routes
 
 export const createProduct = async(req,res,next)=>{
-    console.log(req.body, 'incoming request body'); 
     const newProduct = req.body;
-    console.log(newProduct, 'newProduct request body'); 
     try {
         await Product.create(newProduct)
         res.status(201).json({
@@ -13,6 +11,74 @@ export const createProduct = async(req,res,next)=>{
             newProduct
         })
         
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:`problem due to ${error}`
+        })
+    }
+}
+
+// Get all Product
+
+export const getProducts = async(req,res,next)=>{
+    try {
+      const allProduct =   await Product.find({})
+        res.status(200).json({
+            success:true,
+            allProduct
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:`problem due to ${error}`
+        })
+    }
+}
+
+// Update a Product -------ADMIN routes
+
+export const updateProduct = async(req,res,next)=>{
+    try {
+        const existingProduct = await Product.findById(req.params.id);
+        if(!existingProduct){
+           return res.status(404).json({
+                success:false,
+                message:"Product is not Found"
+            })
+        }
+
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        res.status(200).json({
+            success:true,
+            updatedProduct
+        })
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:`problem due to ${error}`
+        })
+    }
+}
+
+// Update a Product -------ADMIN routes
+
+export const deleteProduct = async(req,res,next)=>{
+    try {
+        const existingProduct = await Product.findById(req.params.id);
+        if(!existingProduct){
+           return res.status(404).json({
+                success:false,
+                message:"Product is not Found"
+            })
+        }
+
+        await Product.findByIdAndDelete(existingProduct);
+        res.status(200).json({
+            success:true,
+            message:"Product deleted successfully"
+        })
     } catch (error) {
         res.status(500).json({
             success:false,
